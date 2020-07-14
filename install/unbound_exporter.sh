545fbe4cd673bf127dc562a95f190e6796b4efd9
Phillip R. Jaenke <prj@rootwyrm.com>
2020-07-14 13:07:12 -0400

    add some exporters... nsd_exporter is broken

--- install/unbound_exporter.sh
+++ install/unbound_exporter.sh
@@ -19,40 +19,41 @@ fi
 #	exit 1
 #fi
 
-if [ ! -d nsd_exporter ]; then
-	git clone https://github.com/optix2000/nsd_exporter.git
+if [ ! -d unbound_exporter ]; then
+	git clone https://github.com/kumina/unbound_exporter.git
 	if [ $? -ne 0 ]; then
-		printf 'Failed to clone from optix2000/nsd_exporter\n'
+		printf 'Failed to clone from kumina/unbound_exporter\n'
 		exit 1
 	fi
 else
-	git pull -r true nsd_exporter
+	git pull -r true unbound_exporter
 fi
 
 ## XXX: won't build on rhel/centos, needs to be built in an alpine docker
-install_nsd()
+install_unbound()
 {
-	docker run -i -v nsd_exporter:/nsd_exporter alpine:latest /bin/sh << 'EOF'
+	## XXX: don't use the build_static.sh script because it uses edge.
+	docker run --rm -i -v unbound_exporter:/unbound_exporter alpine:latest /bin/sh << 'EOF'
 set -ex
 apk update
 apk add ca-certificates git go go-bindata libc-dev make binutils
-cd /nsd_exporter
-make
-strip nsd_exporter
+cd /unbound_exporter
+go build --ldflags '-extldflags "-static"'
+strip unbound_exporter
 EOF
 	if [ $? -ne 0 ]; then
 		printf 'There was an issue compiling!\n'
 		exit 1
 	fi
 	## This is to be run after the container is built, so.
-	docker cp nsd_exporter/nsd_exporter nsd:/opt/rootwyrm/bin/nsd_exporter
+	docker cp unbound_exporter/unbound_exporter unbound:/opt/rootwyrm/bin/unbound_exporter
 	if [ $? -ne 0 ]; then
 		printf 'Error copying into the container!\n'
 		printf 'Did you docker-compose up --no-start first?\n'
 		exit 1
 	fi
-	docker cp 
-	#docker exec -it nsd rc-update add nsd_exporter
+	docker cp init.d/unbound_exporter unbound:/etc/init.d/unbound_exporter
+	docker exec -it unbound rc-update add unbound_exporter
 	exit 0
 }
 
@@ -60,12 +61,18 @@ EOF
 printf 'ATTENTION: You must agree to the following license to install this\n'
 printf 'software. Read carefully before accepting.\n'
 printf '\n'
-cat nsd_exporter/LICENSE
+printf 'Press any key to continue to the license or ^C to decline\n'
+while [ true ]; do
+	read -t 3 -n 1
+done
+cat unbound_exporter/LICENSE | more
 printf '\n'
 printf 'Press any key to accept the license or ^C to decline\n'
 while [ true ]; do
 	read -t 3 -n 1
 	if [ $? = 0 ]; then
-		install_nsd
+		install_unbound
+	else
+		exit 0
 	fi
 done
