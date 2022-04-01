175dbc13d5336997916529bab47375deb3a36478
Phillip R. Jaenke <prj@rootwyrm.com>
2022-04-01 00:39:53 -0400

    Update license dockerfile

--- container/bind/application/build/11.bind_exporter.sh
+++ container/bind/application/build/11.bind_exporter.sh
@@ -16,65 +16,28 @@ ingest_environment
 software_version
 
 export BUILDNAME="bind_exporter"
-export GOPATH="/usr/local/go"
-export GOURL="github.com/prometheus-community/bind_exporter"
 
-## Build
-export vbpkg="${BUILDNAME}_build"
-export vbpkg_content="go git make gcc g++"
+export BIND_EXPORTER_VER="${BIND_EXPORTER_VER:-0.5.0}"
 
 ## busybox doesn't support nanoseconds
 export DATEFMT="+%FT%T%z"
 export curl_cmd="/usr/bin/curl --tlsv1.2 -L --silent"
 
-install_buildpkg()
+install_package()
 {
-	######################################################################
-	## Install our build packages.
-	######################################################################
-	echo "$(date $DATEFMT) [${BUILDNAME}] Entering build phase."
-	echo "$(date $DATEFMT) [${BUILDNAME}] Installing build dependencies as $vbpkg"
-	/sbin/apk --no-cache add --virtual $vbpkg $vbpkg_content > /dev/null 2>&1 
-	CHECK_ERROR $? $vbpkg
-}
-
-################################################################################
-# Actual build routine
-################################################################################
-build()
-{
-	if [ ! -d $GOPATH ]; then
-		mkdir -p $GOPATH
+	local BINDIR=/opt/local/bind_exporter
+	local ARCH=$(uname -m)
+	local URL=https://github.com/prometheus-community/bind_exporter/releases/download/v${BIND_EXPORTER_VER}/bind_exporter-${BIND_EXPORTER_VER}.linux-${ARCH}.tar.gz
+	curl -o /tmp/bind_exporter.tgz $URL
+	if [ ! -d $BINDIR ]; then
+		mkdir $BINDIR
 	fi
-	echo "$(date $DATEFMT) [${BUILDNAME}] adding bind_exporter..."
-	go get -v $GOURL
-	CHECK_ERROR $? bind_exporter_go_get
-	cd $GOPATH/src/$GOURL
-	make
-	if [ $? -eq 0 ]; then
-		mv $GOPATH/bin/bind_exporter /usr/local/sbin/
-		mv $GOPATH/bin/promu /usr/local/sbin/
-		rc-update add bind_exporter
-	fi
+	tar -c ${BINDIR} --strip-components=1 -zxf /tmp/bind_exporter.tgz "bind_exporter-${BIND_EXPORTER_VER}.linux-${ARCH}/LICENSE"
+	tar -c ${BINDIR} --strip-components=1 -zxf /tmp/bind_exporter.tgz "bind_exporter-${BIND_EXPORTER_VER}.linux-${ARCH}/NOTICE"
+	tar -c ${BINDIR} --strip-components=1 -zxf /tmp/bind_exporter.tgz "bind_exporter-${BIND_EXPORTER_VER}.linux-${ARCH}/bind_exporter"
 }
 
-clean()
-{
-	######################################################################
-	## Clean up after ourselves.
-	######################################################################
-	echo "$(date $DATEFMT) [${BUILDNAME}] Cleaning up build."
-	cd /root
-	rm -rf $GOPATH/bin
-	rm -rf $GOPATH/src
-	rm -rf $GOPATH/pkg
-	/sbin/apk --no-cache del $vbpkg
-	CHECK_ERROR $? "${BUILDNAME}_clean_apk"
-}
-
-install_buildpkg
-build
-clean
+install_package
 
 echo "$(date $DATEFMT) [${BUILDNAME}] Build and install of ${DISTVER} complete."
 exit 0 
